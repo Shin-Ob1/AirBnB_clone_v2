0201399caf3089975ec1b60395f07cbe0ce88e13
ShinOb1 <obikelv@gmail.com>
2023-10-08 19:28:27 -0700

    debug 2 deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,26 +10,45 @@ env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
-        return False
-    try:
-        filename = os.path.basename(archive_path)
-        dfile = filename.split(".")[0]
-        put(archive_path, "/tmp/")
-        sudo("mkdir -p /data/web_static/releases/{}/".format(dfile))
-        sudo("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-             .format(filename, dfile))
-        sudo("rm /tmp/{}".format(filename))
-        sudo(
-            "mv /data/web_static/releases/{}/web_static/* "
-            "/data/web_static/releases/{}/"
-            .format(dfile, dfile))
-        sudo("rm -rf /data/web_static/releases/{}/web_static"
-             .format(dfile))
-        sudo("rm -rf /data/web_static/current")
-        sudo("ln -s /data/web_static/releases/{} /data/web_static/current"
-             .format(dfile))
+        """Deploy web files to server
+        """
+        try:
+                if not (path.exists(archive_path)):
+                        return False
+
+                # upload archive
+                put(archive_path, '/tmp/')
+
+                # create target dir
+                timestamp = archive_path[-18:-4]
+                run('sudo mkdir -p /data/web_static/\
+releases/web_static_{}/'.format(timestamp))
+
+                # uncompress archive and delete .tgz
+                run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
+/data/web_static/releases/web_static_{}/'
+                    .format(timestamp, timestamp))
+
+                # remove archive
+                run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
+
+                # move contents into host web_static
+                run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
+/data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
+
+                # remove extraneous web_static dir
+                run('sudo rm -rf /data/web_static/releases/\
+web_static_{}/web_static'
+                    .format(timestamp))
+
+                # delete pre-existing sym link
+                run('sudo rm -rf /data/web_static/current')
+
+                # re-establish symbolic link
+                run('sudo ln -s /data/web_static/releases/\
+web_static_{}/ /data/web_static/current'.format(timestamp))
+        except:
+                return False
+
+        # return True on success
         return True
-    except Exception:
-        return False
